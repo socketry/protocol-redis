6b13a2e844afcf54ead56b297e915ae981355448
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-08-16 17:08:21 +1200

    Move methods from async-redis.

---
+++ lib/protocol/redis/methods.rb
@@ -0,0 +1,39 @@
+# Copyright, 2019, by Mikael Henriksson. <http://www.mhenrixon.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require_relative 'methods/hashes'
+require_relative 'methods/keys'
+require_relative 'methods/lists'
+require_relative 'methods/server'
+require_relative 'methods/strings'
+
+module Protocol
+	module Redis
+		module Methods
+			def self.included(klass)
+				klass.include Methods::Hashes
+				klass.include Methods::Keys
+				klass.include Methods::Lists
+				klass.include Methods::Server
+				klass.include Methods::Strings
+			end
+		end
+	end
+end
