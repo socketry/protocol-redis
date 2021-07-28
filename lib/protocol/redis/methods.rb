4edf9b754b9761755a40b7592322a146d19034a3
Daniel Evans <daniel.evans@airbnb.com>
2021-07-28 15:19:03 -0600

    Include sets and streams in the protocol methods

--- lib/protocol/redis/methods.rb
+++ lib/protocol/redis/methods.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 # Copyright, 2019, by Mikael Henriksson. <http://www.mhenrixon.com>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,7 +29,9 @@ require_relative 'methods/counting'
 
 require_relative 'methods/hashes'
 require_relative 'methods/lists'
+require_relative 'methods/sets'
 require_relative 'methods/strings'
+require_relative 'methods/streams'
 require_relative 'methods/sorted_sets'
 
 require_relative 'methods/pubsub'
@@ -42,14 +44,16 @@ module Protocol
 				klass.include Methods::Connection
 				klass.include Methods::Server
 				klass.include Methods::Geospatial
-				
+
 				klass.include Methods::Counting
-				
+
 				klass.include Methods::Hashes
 				klass.include Methods::Lists
+				klass.include Methods::Sets
 				klass.include Methods::SortedSets
 				klass.include Methods::Strings
-				
+				klass.include Methods::Streams
+
 				klass.include Methods::Pubsub
 			end
 		end
