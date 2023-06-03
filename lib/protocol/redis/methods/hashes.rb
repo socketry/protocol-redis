49b109e5ab6b16b2fcb68f66ccdc966319ae6c7f
Sergey B (Troex Nevelin) <436912+troex@users.noreply.github.com>
2023-06-03 14:22:43 +0200

    Make hashes methods compatible with redis-rb. (#12)

--- lib/protocol/redis/methods/hashes.rb
+++ lib/protocol/redis/methods/hashes.rb
@@ -10,6 +10,7 @@ module Protocol
 				# Get the number of fields in a hash. O(1).
 				# @see https://redis.io/commands/hlen
 				# @param key [Key]
+				# @return [Integer]
 				def hlen(key)
 					call('HLEN', key)
 				end
@@ -17,6 +18,7 @@ module Protocol
 				# Set the string value of a hash field. O(1) for each field/value pair added, so O(N) to add N field/value pairs when the command is called with multiple field/value pairs.
 				# @see https://redis.io/commands/hset
 				# @param key [Key]
+				# @return [Integer] if new field added returns "1" otherwise "0"
 				def hset(key, field, value)
 					call('HSET', key, field, value)
 				end
@@ -26,13 +28,15 @@ module Protocol
 				# @param key [Key]
 				# @param field [String]
 				# @param value [String]
+				# @return [Boolean] "true" if new field added, "false" otherwise
 				def hsetnx(key, field, value)
-					call('HSETNX', key, field, value)
+					call('HSETNX', key, field, value) > 0
 				end
 				
 				# Set multiple hash fields to multiple values. O(N) where N is the number of fields being set.
 				# @see https://redis.io/commands/hmset
 				# @param key [Key]
+				# @return [String] default: "OK"
 				def hmset(key, *attrs)
 					call('HMSET', key, *attrs)
 				end
@@ -41,6 +45,7 @@ module Protocol
 				# @see https://redis.io/commands/hget
 				# @param key [Key]
 				# @param field [String]
+				# @return [String, Null]
 				def hget(key, field)
 					call('HGET', key, field)
 				end
@@ -49,14 +54,16 @@ module Protocol
 				# @see https://redis.io/commands/hmget
 				# @param key [Key]
 				# @param field [String]
-				def hmget(key, *fields, &blk)
-					call('HMGET', key, *fields, &blk)
+				# @return [Array]
+				def hmget(key, *fields)
+					call('HMGET', key, *fields)
 				end
 				
 				# Delete one or more hash fields. O(N) where N is the number of fields to be removed.
 				# @see https://redis.io/commands/hdel
 				# @param key [Key]
 				# @param field [String]
+				# @return [Integer] number of deleted fields
 				def hdel(key, *fields)
 					call('HDEL', key, *fields)
 				end
@@ -65,8 +72,9 @@ module Protocol
 				# @see https://redis.io/commands/hexists
 				# @param key [Key]
 				# @param field [String]
+				# @return [Boolean]
 				def hexists(key, field)
-					call('HEXISTS', key, field)
+					call('HEXISTS', key, field) > 0
 				end
 				
 				# Increment the integer value of a hash field by the given number. O(1).
@@ -74,6 +82,7 @@ module Protocol
 				# @param key [Key]
 				# @param field [String]
 				# @param increment [Integer]
+				# @return [Integer] field value after increment
 				def hincrby(key, field, increment)
 					call('HINCRBY', key, field, increment)
 				end
@@ -83,13 +92,15 @@ module Protocol
 				# @param key [Key]
 				# @param field [String]
 				# @param increment [Double]
+				# @return [Float] field value after increment
 				def hincrbyfloat(key, field, increment)
-					call('HINCRBYFLOAT', key, field, increment)
+					Float(call('HINCRBYFLOAT', key, field, increment))
 				end
 				
 				# Get all the fields in a hash. O(N) where N is the size of the hash.
 				# @see https://redis.io/commands/hkeys
 				# @param key [Key]
+				# @return [Array]
 				def hkeys(key)
 					call('HKEYS', key)
 				end
@@ -97,6 +108,7 @@ module Protocol
 				# Get all the values in a hash. O(N) where N is the size of the hash.
 				# @see https://redis.io/commands/hvals
 				# @param key [Key]
+				# @return [Array]
 				def hvals(key)
 					call('HVALS', key)
 				end
@@ -104,8 +116,9 @@ module Protocol
 				# Get all the fields and values in a hash. O(N) where N is the size of the hash.
 				# @see https://redis.io/commands/hgetall
 				# @param key [Key]
+				# @return [Hash]
 				def hgetall(key)
-					call('HGETALL', key)
+					call('HGETALL', key).each_slice(2).to_h
 				end
 			end
 		end
